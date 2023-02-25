a0e8d0ffdcbba094747d2b22162760293de05423
Nikita Leontev <121257735+InYnI@users.noreply.github.com>
2023-02-25 15:01:16 +0300

    Update Patrol.cs

--- Assets/Scripts/Enemy/Patrol.cs
+++ Assets/Scripts/Enemy/Patrol.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Enemy))]
 public class Patrol : MonoBehaviour
 {
     [SerializeField] private Transform _path;
