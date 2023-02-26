aa3c6e86d1840180ecdae66facb0ebf66557671b
Nikita Leontev <121257735+InYnI@users.noreply.github.com>
2023-02-26 14:50:57 +0300

    Update EndLevel.cs

--- Assets/Scripts/EndLevel.cs
+++ Assets/Scripts/EndLevel.cs
@@ -5,7 +5,7 @@ public class EndLevel : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.TryGetComponent<PlayerController>(out PlayerController playerController))
+        if (collision.TryGetComponent<PlayerActions>(out PlayerActions playerActions))
         {
             SceneManager.LoadScene("FirstLevelScene");
         }
